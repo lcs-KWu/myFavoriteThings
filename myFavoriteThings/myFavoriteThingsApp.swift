9d9ca4faee50d547b25e35a813bb5302940f994b
Kevin Wu <KWu@lcs.on.ca>
2024-11-22 08:47:41 -0500

    Initial Commit

---
+++ myFavoriteThings/myFavoriteThingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  myFavoriteThingsApp.swift
+//  myFavoriteThings
+//
+//  Created by Yixuan Wu on 2024-11-22.
+//
+
+import SwiftUI
+
+@main
+struct myFavoriteThingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
