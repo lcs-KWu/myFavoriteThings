e90071f41ef261f0a9c231001db8fd47a9b65f51
Kevin Wu <KWu@lcs.on.ca>
2024-11-28 20:30:33 -0500

    finnished setting up the detailed view

---
+++ myFavoriteThings/FavouriteThingsdetailed.swift
@@ -0,0 +1,18 @@
+//
+//  FavouriteThingsdetailed.swift
+//  myFavoriteThings
+//
+//  Created by Yixuan Wu on 2024-11-28.
+//
+
+import SwiftUI
+
+struct FavouriteThingsdetailed: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavouriteThingsdetailed()
+}
